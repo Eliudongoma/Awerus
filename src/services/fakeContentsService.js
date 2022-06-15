25cfa75f28e78d7fd16691e68a7c851c3d58ae7b
Augustine Awuori <augustineawuori95@gmail.com>
2022-06-15 20:29:25 +0300

    Update contents.

--- src/services/fakeContentsService.js
+++ src/services/fakeContentsService.js
@@ -4,6 +4,11 @@ const contents = [
   { heading: "Goodnight in Swahili" },
   { heading: "Other useful phrases that will come in handy" },
   { heading: "Tarakimu" },
+  { heading: "Siku za Wiki (Days of the week)" },
+  { heading: "Panga Siku za Wiki" },
+  { heading: "Zoezi" },
+  { heading: "Familia (Familia)" },
+  { heading: "Sehemu za Mwili" },
 ];
 
 const getContents = () => contents;
